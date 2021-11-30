280b2f451faa3f60b1422e4f65d946b1507389d9
Filip Lončar <filoncar@vsite.hr>
2021-11-30 23:32:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+Napraviti GUI program koji će odbrojavati vrijeme za neki početno zadani period (npr. 
+odbrojavati trajanje ispita) i prikazivati preostalo vrijeme u minutama preko cijelog ekrana. 
+Zadnjih 5 minuta brojevi moraju treptati kao upozorenje za skori istek vremena. Opcionalno 
+dodati zvučne signale za 5 minuta prije kraja i za istek vremena.
+
+ */
+
+namespace Timer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
